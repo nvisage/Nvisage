91e73dc1ef8c063af58c50a597594b5edcf1e245
greenkee <justinlin98@gmail.com>
2016-01-31 01:23:19 -0800

    Eraser and better text box placement

--- drawing.js
+++ drawing.js
@@ -1,7 +1,7 @@
 //registers div as canvas - for IE?
 var canvasDiv = document.getElementById('canvasDiv');
 canvas = document.createElement('canvas');
-canvas.setAttribute('width', 400);
+canvas.setAttribute('width', 600);
 canvas.setAttribute('height', 400);
 canvas.setAttribute('id', 'canvas');
 canvas.setAttribute("styles", 'position:relative, z-index:0')
@@ -16,27 +16,32 @@ context = canvas.getContext("2d");
 //drawing functions
 var paint = false;
 var isDrawMode = true;
+var isErasing = false;
 
 $('#canvas').mousedown(function(e) {
   var mouseX = e.pageX - this.offsetLeft;
   var mouseY = e.pageY - this.offsetTop;
-  if (!isDrawMode) {
-    var motionContainer = $("<div></div>", {
-      "class": "Contain"
-    });
-    var textBox = $("<textarea>Test</textarea>", {
-      "class": "TextBox",
-      "left": mouseX,
-      "top": mouseY
-    });
-    motionContainer.append(textBox);
-    motionContainer.draggable().resizable();
-    $("#canvasDiv").append(motionContainer);
-  }
   if (isDrawMode) {
     paint = true;
     addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
     redraw();
+  }
+  if (!isDrawMode) {
+    var locationContainer = $("<div></div>");
+    locationContainer.addClass("Contain");
+    locationContainer.css("left", mouseX);
+    locationContainer.css("top", mouseY);
+    var motionContainer = $("<div></div>");
+    motionContainer.addClass("MoveBox");
+    var textBox = $("<textarea>Enter Text</textarea>");
+    textBox.addClass("TextBox")
+    motionContainer.append(textBox);
+    motionContainer.draggable().resizable();
+    locationContainer.append(motionContainer);
+    $("#canvasDiv").append(locationContainer);
+
+    //turns back to drawing
+    changeMode();
   }
 });
 
@@ -71,7 +76,6 @@ var clickColor = new Array();
 function addClick(x, y, dragging)
 {
   text = $('#colorPicker').val();
-  console.log(text);
   clickX.push(x);
   clickY.push(y);
   clickDrag.push(dragging);
@@ -84,7 +88,7 @@ function redraw() {
   context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
 
   context.lineJoin = "round";
-  context.lineWidth = 5;
+  //context.lineWidth = 5;
 
   for(var i=0; i < clickX.length; i++)
   {
@@ -96,7 +100,13 @@ function redraw() {
     }
     context.lineTo(clickX[i], clickY[i]);
     context.closePath();
-    context.strokeStyle = clickColor[i];
+    var currColor = clickColor[i];
+    if(currColor == "white"){
+      context.lineWidth = 20;
+    }else{
+      context.lineWidth = 5;
+    }
+    context.strokeStyle = currColor;
     context.stroke();
   }
 }
@@ -116,12 +126,13 @@ $('#saveButton').click(function() {
 })
 
 //switches between draw and text
-$('#switchButton').click(function() {
-  console.log("SWITCH");
+function changeMode(){
   isDrawMode = !isDrawMode;
   if (isDrawMode) {
-    $(this).text("DRAWING");
+    $('#switchButton').text("Add Text Box");
   } else {
-    $(this).text("TYPING");
+    $('#switchButton').text("Click To Place");
   }
-})
+}
+
+$('#switchButton').click(changeMode);
